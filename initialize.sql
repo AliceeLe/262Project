9bbc2318f049bbc08081a01dd2f8a10d9e72f5c1
AliceeLe <anhle@andrew.cmu.edu>
2024-11-22 14:45:50 -0500

    Modify table names in initalize.sql

--- initialize.sql
+++ initialize.sql
@@ -10,6 +10,6 @@ CREATE database Discord;
 
 -- load the data
 
-\copy Users (user_ID, user_name) FROM data/users.csv csv header;
-\copy Messages (message_id, message_content, time, is_Pinned, cid, uid) FROM data/issues.csv csv header;
-\copy Channel (channel_id, message_id) FROM data/bugs.csv csv header;
+\copy Users (user_ID, user_name) FROM data/user.csv csv header;
+\copy Messages (message_id, message_content, time, is_Pinned, cid, uid) FROM data/message.csv csv header;
+\copy Channel (channel_id, message_id) FROM data/channel.csv csv header;
