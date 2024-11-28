e309d8d5e9967171dbd2e3b5209e8e97566fe496
张诗雨 <zsy@zsydeMB.local>
2024-11-28 11:22:43 -0500

    create.sql update

--- create.sql
+++ create.sql
@@ -1,28 +1,28 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2024-11-27 20:25:22.684
+-- Last modification date: 2024-11-28 16:21:26.862
 
 -- tables
 -- Table: APIDevelopment
--- CREATE TABLE APIDevelopment (
---     channel_id int  NOT NULL,
---     bots_id int  NOT NULL,
---     CONSTRAINT APIDevelopment_pk PRIMARY KEY (bots_id,channel_id)
--- );
+CREATE TABLE APIDevelopment (
+    channel_id int  NOT NULL,
+    bots_id int  NOT NULL,
+    CONSTRAINT APIDevelopment_pk PRIMARY KEY (bots_id,channel_id)
+);
 
 -- Table: Ads
--- CREATE TABLE Ads (
---     ads_id int  NOT NULL,
---     revenue int  NOT NULL,
---     partnership text  NOT NULL,
---     CONSTRAINT Ads_pk PRIMARY KEY (ads_id)
--- );
+CREATE TABLE Ads (
+    ads_id int  NOT NULL,
+    revenue int  NOT NULL,
+    tag text  NOT NULL,
+    CONSTRAINT Ads_pk PRIMARY KEY (ads_id)
+);
 
 -- Table: Bots
--- CREATE TABLE Bots (
---     bots_id int  NOT NULL,
---     commands text  NOT NULL,
---     CONSTRAINT Bots_pk PRIMARY KEY (bots_id)
--- );
+CREATE TABLE Bots (
+    bots_id int  NOT NULL,
+    commands text  NOT NULL,
+    CONSTRAINT Bots_pk PRIMARY KEY (bots_id)
+);
 
 -- Table: Channel
 CREATE TABLE Channel (
@@ -34,6 +34,7 @@ CREATE TABLE Channel (
 -- Table: Members
 CREATE TABLE Members (
     user_id int  NOT NULL,
+    is_muted boolean  NOT NULL,
     CONSTRAINT Members_pk PRIMARY KEY (user_id)
 );
 
@@ -56,26 +57,26 @@ CREATE TABLE Messages (
 );
 
 -- Table: Moderator
--- CREATE TABLE Moderator (
---     user_id int  NOT NULL,
---     CONSTRAINT Moderator_pk PRIMARY KEY (user_id)
--- );
+CREATE TABLE Moderator (
+    user_id int  NOT NULL,
+    CONSTRAINT Moderator_pk PRIMARY KEY (user_id)
+);
 
 -- Table: Partnership
--- CREATE TABLE Partnership (
---     channel_id int  NOT NULL,
---     ads_id int  NOT NULL,
---     CONSTRAINT Partnership_pk PRIMARY KEY (channel_id,ads_id)
--- );
+CREATE TABLE Partnership (
+    channel_id int  NOT NULL,
+    ads_id int  NOT NULL,
+    CONSTRAINT Partnership_pk PRIMARY KEY (channel_id,ads_id)
+);
 
 -- Table: Permisson
--- CREATE TABLE Permisson (
---     user_id int  NOT NULL,
---     server_id int  NOT NULL,
---     able_to_customize boolean  NOT NULL,
---     able_to_mute boolean  NOT NULL,
---     CONSTRAINT Permisson_pk PRIMARY KEY (user_id,server_id)
--- );
+CREATE TABLE Permisson (
+    user_id int  NOT NULL,
+    server_id int  NOT NULL,
+    able_customize boolean  NOT NULL,
+    able_mute boolean  NOT NULL,
+    CONSTRAINT Permisson_pk PRIMARY KEY (user_id,server_id)
+);
 
 -- Table: Posts
 CREATE TABLE Posts (
@@ -95,6 +96,7 @@ CREATE TABLE Servers (
     tag text  NOT NULL,
     num_of_member int  NOT NULL,
     region text  NOT NULL,
+    theme text  NOT NULL,
     CONSTRAINT Servers_pk PRIMARY KEY (server_id)
 );
 
@@ -107,20 +109,20 @@ CREATE TABLE Users (
 
 -- foreign keys
 -- Reference: APIDevelopment_Bots (table: APIDevelopment)
--- ALTER TABLE APIDevelopment ADD CONSTRAINT APIDevelopment_Bots
---     FOREIGN KEY (bots_id)
---     REFERENCES Bots (bots_id)  
---     NOT DEFERRABLE 
---     INITIALLY IMMEDIATE
--- ;
+ALTER TABLE APIDevelopment ADD CONSTRAINT APIDevelopment_Bots
+    FOREIGN KEY (bots_id)
+    REFERENCES Bots (bots_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
 
 -- Reference: APIDevelopment_Channel (table: APIDevelopment)
--- ALTER TABLE APIDevelopment ADD CONSTRAINT APIDevelopment_Channel
---     FOREIGN KEY (channel_id)
---     REFERENCES Channel (channel_id)  
---     NOT DEFERRABLE 
---     INITIALLY IMMEDIATE
--- ;
+ALTER TABLE APIDevelopment ADD CONSTRAINT APIDevelopment_Channel
+    FOREIGN KEY (channel_id)
+    REFERENCES Channel (channel_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
 
 -- Reference: Channel_Messages (table: Messages)
 ALTER TABLE Messages ADD CONSTRAINT Channel_Messages
@@ -138,29 +140,29 @@ ALTER TABLE Messages ADD CONSTRAINT Messages_Users
     INITIALLY IMMEDIATE
 ;
 
--- -- Reference: Partnership_Ads (table: Partnership)
--- ALTER TABLE Partnership ADD CONSTRAINT Partnership_Ads
---     FOREIGN KEY (ads_id)
---     REFERENCES Ads (ads_id)  
---     NOT DEFERRABLE 
---     INITIALLY IMMEDIATE
--- ;
+-- Reference: Partnership_Ads (table: Partnership)
+ALTER TABLE Partnership ADD CONSTRAINT Partnership_Ads
+    FOREIGN KEY (ads_id)
+    REFERENCES Ads (ads_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
 
--- -- Reference: Partnership_Channel (table: Partnership)
--- ALTER TABLE Partnership ADD CONSTRAINT Partnership_Channel
---     FOREIGN KEY (channel_id)
---     REFERENCES Channel (channel_id)  
---     NOT DEFERRABLE 
---     INITIALLY IMMEDIATE
--- ;
+-- Reference: Partnership_Channel (table: Partnership)
+ALTER TABLE Partnership ADD CONSTRAINT Partnership_Channel
+    FOREIGN KEY (channel_id)
+    REFERENCES Channel (channel_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
 
--- -- Reference: Permisson_Moderator (table: Permisson)
--- ALTER TABLE Permisson ADD CONSTRAINT Permisson_Moderator
---     FOREIGN KEY (user_id)
---     REFERENCES Moderator (user_id)  
---     NOT DEFERRABLE 
---     INITIALLY IMMEDIATE
--- ;
+-- Reference: Permisson_Moderator (table: Permisson)
+ALTER TABLE Permisson ADD CONSTRAINT Permisson_Moderator
+    FOREIGN KEY (user_id)
+    REFERENCES Moderator (user_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
 
 -- Reference: Posts_Channel (table: Posts)
 ALTER TABLE Posts ADD CONSTRAINT Posts_Channel
@@ -178,28 +180,28 @@ ALTER TABLE Channel ADD CONSTRAINT Servers_Channel
     INITIALLY IMMEDIATE
 ;
 
--- -- Reference: Servers_Permisson (table: Permisson)
--- ALTER TABLE Permisson ADD CONSTRAINT Servers_Permisson
---     FOREIGN KEY (server_id)
---     REFERENCES Servers (server_id)  
---     NOT DEFERRABLE 
---     INITIALLY IMMEDIATE
--- ;
+-- Reference: Servers_Permisson (table: Permisson)
+ALTER TABLE Permisson ADD CONSTRAINT Servers_Permisson
+    FOREIGN KEY (server_id)
+    REFERENCES Servers (server_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
 
--- -- Reference: Users_Members (table: Members)
--- ALTER TABLE Members ADD CONSTRAINT Users_Members
---     FOREIGN KEY (user_id, user_id)
---     REFERENCES Users (user_id, user_name)  
---     NOT DEFERRABLE 
---     INITIALLY IMMEDIATE
--- ;
+-- Reference: Users_Members (table: Members)
+ALTER TABLE Members ADD CONSTRAINT Users_Members
+    FOREIGN KEY (user_id, user_id)
+    REFERENCES Users (user_id, user_name)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
 
--- -- Reference: Users_Moderator (table: Moderator)
--- ALTER TABLE Moderator ADD CONSTRAINT Users_Moderator
---     FOREIGN KEY (user_id)
---     REFERENCES Users (user_name)  
---     NOT DEFERRABLE 
---     INITIALLY IMMEDIATE
+-- Reference: Users_Moderator (table: Moderator)
+ALTER TABLE Moderator ADD CONSTRAINT Users_Moderator
+    FOREIGN KEY (user_id)
+    REFERENCES Users (user_name)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
 ;
 
 -- Reference: Users_Posts (table: Posts)
@@ -210,8 +212,8 @@ ALTER TABLE Posts ADD CONSTRAINT Users_Posts
     INITIALLY IMMEDIATE
 ;
 
--- Reference: Membership_Servers (table: Membership)
-ALTER TABLE Membership ADD CONSTRAINT Membership_Servers
+-- Reference: membership_Servers (table: Membership)
+ALTER TABLE Membership ADD CONSTRAINT membership_Servers
     FOREIGN KEY (server_id)
     REFERENCES Servers (server_id)  
     NOT DEFERRABLE 
